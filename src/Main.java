766e6e938dd7ceec270c310487ea4ec5f54080b9
HarleyY12 <hyang2496@gmail.com>
2025-05-07 18:38:25 -0400

    Instantiated variables

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MazeSolver newMazeSolver = new MazeSolver();
+        String[][]maze = getMaze("src/file");
+        newMazeSolver.solveMaze(maze);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+
     }
+
+    public static String[][] getMaze(String fileName) {
+        File f = new File(fileName);
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
+
+        ArrayList<String> fileData = new ArrayList<String>();
+        while (s.hasNextLine())
+            fileData.add(s.nextLine());
+
+        int rows = fileData.size();
+        int cols = fileData.get(0).length();
+
+        String[][] maze = new String[rows][cols];
+
+        for (int i = 0; i < fileData.size(); i++) {
+            String d = fileData.get(i);
+            for (int j = 0; j < d.length(); j++) {
+                maze[i][j] = d.charAt(j) + "";
+            }
+        }
+        return maze;
+
+    }
+
 }
--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class MazeSolver {
+    public void solveMaze(String [][] maze){
+        int row = maze.length;
+        int column = maze[0].length;
+        int xPos = 0;
+        int yPos = 0;
+        boolean[][]visitedPositions = new boolean[row][column];
+
+
+
+
+    }
 }
